58bf7cb41b2766e7362cd76ebcf7121930f082a1
SWAPNIL SRIVASTAVA <75637474+STRYKER2403@users.noreply.github.com>
2022-09-20 04:46:30 +0530

    state 27-30

--- src/components/News.js
+++ src/components/News.js
@@ -1,84 +1,101 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
-    articles = [
-      {
-          "source": {
-              "id": "espn-cric-info",
-              "name": "ESPN Cric Info"
-          },
-          "author": null,
-          "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-          "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-          "publishedAt": "2020-04-27T11:41:47Z",
-          "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-      },
-      {
-          "source": {
-              "id": "espn-cric-info",
-              "name": "ESPN Cric Info"
-          },
-          "author": null,
-          "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-          "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-          "publishedAt": "2020-03-30T15:26:05Z",
-          "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-      },
-      {
-          "source": {
-              "id": null,
-              "name": "Northeast Now"
-          },
-          "author": "NE NOW NEWS",
-          "title": "Garena Free Fire Redeem Codes for September 19 2022 : Get Free Bundles, Skins, And Many More - Northeast Now",
-          "description": "Garena Free Fire is the most exciting battle royale game, which gained a lot of popularity in India during the last few weeks, especially in absence of Pubg Mobile India.",
-          "url": "http://nenow.in/free-fire/garena-free-fire-redeem-codes-for-september-19-2022-get-free-bundles-skins-and-many-more.html",
-          "urlToImage": "https://nenow.in/wp-content/uploads/2022/03/Free-Fire-Redeem-Code.jpg",
-          "publishedAt": "2022-09-19T02:19:37Z",
-          "content": "Garena Free Fire is the most exciting battle royale game, which gained a lot of popularity in India during the last few weeks, especially in absence of Pubg Mobile India.\r\nNow it is becoming one of t… [+1981 chars]"
-      },
-      {
-          "source": {
-              "id": null,
-              "name": "NDTV News"
-          },
-          "author": null,
-          "title": "Queen Elizabeth's Funeral Today, World Leaders In Attendance: 10 Points - NDTV",
-          "description": "Queen Elizabeth's hour-long state funeral, the first in Britain since the death of her first prime minister Winston Churchill in 1965, will take place today at Westminster Abbey in London.",
-          "url": "https://www.ndtv.com/world-news/queen-elizabeth-queen-elizabeth-funeral-queen-elizabeths-funeral-today-world-leaders-in-attendance-10-points-3356494",
-          "urlToImage": "https://c.ndtvimg.com/2022-09/4e2aurq8_queen_625x300_19_September_22.jpg",
-          "publishedAt": "2022-09-19T01:47:51Z",
-          "content": "<li>The funeral will bring to an end 11 days of national mourning across the United Kingdom that has seen the personal sorrow of the royal family play out in the glare of intense international attent… [+2087 chars]"
-      }
-  ]
+    
+    static defaultProps = {
+        country: 'in',
+        pagesize: 8,
+        category: "general"
+    }
+
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string
+
+    }
 
     constructor(){
         super();
         console.log("hello constructor")
         this.state = {
-             articles: this.articles,
-             loading: false
+             articles: [],
+             loading: false,
+             page:1
         }
     }
 
+    async componentDidMount(){
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8930c85f3d514c8e97f5680deeb003a9&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({loading:true});
+        let data = await fetch(url);
+        let parsedata = await data.json();
+        // console.log(parsedata);
+        this.setState({
+            articles : parsedata.articles,
+            totalResults : parsedata.totalResults,
+            loading:false
+        });
+    }
+    
+    HandlePrev = async ()=>{
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category =${this.props.category}&apiKey=8930c85f3d514c8e97f5680deeb003a9&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
+        this.setState({loading:true});
+        let data = await fetch(url);
+        let parsedata = await data.json();
+
+        this.setState({
+            articles : parsedata.articles,
+            page : this.state.page - 1,
+            loading:false
+        });
+
+    }
+
+    HandleNext = async ()=>{
+        if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)))
+        {
+            console.log("Next");
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category =${this.props.category}&apiKey=8930c85f3d514c8e97f5680deeb003a9&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
+            this.setState({loading:true});
+            let data = await fetch(url);
+            let parsedata = await data.json();
+
+            this.setState({
+                articles : parsedata.articles,
+                page : this.state.page + 1,
+                loading:false
+            });
+        }
+    }
+    
+
   render() {
+    
     return (
+
       <div className="container my-3">
-        <h2>NewsCards - Top Headlines</h2>
+        <h2 className='text-center'>NewsCards - Top Headlines</h2>
+
+        {this.state.loading && <Spinner/>}
         
         <div className='row my-3' >
-            {this.state.articles.map((element)=>{
+            {!this.state.loading && this.state.articles.map((element)=>{
                 return <div className='col-md-4' key = {element.url}>
-                <NewsItem title = {element.title.slice(0,45)} description = {element.description.slice(0,88)} ImageUrl = {element.urlToImage} newsUrl = {element.url}/>
+                <NewsItem title = {element.title?element.title.slice(0,45):""} description = {element.description?element.description.slice(0,88):""} ImageUrl = {element.urlToImage} newsUrl = {element.url}/>
                 </div>
             })}
         </div>
-
+        
+        <div className='container d-flex justify-content-between'>
+            <button disabled = {this.state.page<=1} type="button" className="btn btn-dark" onClick={this.HandlePrev}>&larr; Previous</button>
+            <button disabled = {this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.HandleNext}>Next &rarr;</button>
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,12 +6,12 @@ export class NewsItem extends Component {
     let {title,description,ImageUrl,newsUrl} = this.props;
     return (
       <div>
-        <div className="card my-3" style={{width:"18rem"}}>
-        <img src={ImageUrl} className="card-img-top" alt="..."/>
+        <div className="card my-3">
+        <img src={ImageUrl?ImageUrl:"https://resize.indiatvnews.com/en/resize/newbucket/715_-/2022/09/smartwatch-pixabay-1663594523.jpg"} className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target = "_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
+            <a href={newsUrl} target = "_blank" rel="noreferrer" className="btn btn-dark btn-sm btn-primary">Read More</a>
         </div>
         </div>
       </div>
